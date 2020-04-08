20b026960558ac5269600cce43c8dff49fd95721
selandy <se_landy@hotmail.com>
2020-04-07 23:49:44 -0400

    added secondary font support

--- config.def.h
+++ config.def.h
@@ -6,6 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "mono:pixelsize=19:antialias=true:autohint=true";
+static char *font2[] = { "JoyPixels:pixelsize=13:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
