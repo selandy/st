e9bd11ad0716d1cc13056333ebc4b990af6c806b
selandy <se_landy@hotmail.com>
2020-04-08 01:06:24 -0400

    added mouse scrollback with shift

--- config.def.h
+++ config.def.h
@@ -203,6 +203,8 @@ static uint forcemousemod = ShiftMask;
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
+	{ ShiftMask,            Button4, kscrollup,      {.i = 1} },
+	{ ShiftMask,            Button5, kscrolldown,    {.i = 1} },
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
 	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
 };
